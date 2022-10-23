d54efb56030732233d12ae207c21ff6e92c4dcd0
GreggLedoux <guitlouie@gmail.com>
2022-10-23 13:10:20 -0400

    trivia game save

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+let userName = window.prompt ('Please enter you name:')
+
+for (let i = 0; i < questions.lenght; i++) {
+    let question = questions [i]
+    let userAnswer = window.prompt (question.text)
+    if (userAnswer === question.correctAnswer){
+        userScore = userScore + 10
+    }
+}
+
+window.alert ('Your Score is: '+userScore)
 // Your Code Here
